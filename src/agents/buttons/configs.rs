52dffc130a4098e6a5accd87ed709ab7e44a3d5a
Scott Gibb <smgibb@yahoo.com>
2026-05-16 01:07:56 +0100

    refactor: TerminalButtonConfigs
    Signed-off-by: Scott Gibb <smgibb@yahoo.com>

--- src/agents/buttons/configs.rs
+++ src/agents/buttons/configs.rs
@@ -8,13 +8,9 @@ pub struct TerminalButtonConfig {
 
 pub struct TerminalButtonConfigs(Vec<TerminalButtonConfig>);
 
-impl Default for TerminalButtonConfigs {
-    fn default() -> Self {
-        TerminalButtonConfigs(vec![TerminalButtonConfig {
-            key: "start",
-            button: Button::Start,
-            receivers: vec![Addresses::PoliteAgent],
-        }])
+impl TerminalButtonConfigs {
+    pub fn new(configs: Vec<TerminalButtonConfig>) -> Self {
+        Self(configs)
     }
 }
 
--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,23 @@
-use crate::agents::buttons::configs::TerminalButtonConfigs;
+use crate::agents::{
+    Addresses,
+    buttons::{
+        Button,
+        configs::{TerminalButtonConfig, TerminalButtonConfigs},
+    },
+};
 
 pub fn read_terminal_button_configs() -> TerminalButtonConfigs {
     // For simplicity, we hardcode the button configurations here.
     // In a real application, you might read this from a config file or environment variables.
     TerminalButtonConfigs::default()
+}
+
+impl Default for TerminalButtonConfigs {
+    fn default() -> Self {
+        TerminalButtonConfigs::new(vec![TerminalButtonConfig {
+            key: "start",
+            button: Button::Start,
+            receivers: vec![Addresses::PoliteAgent],
+        }])
+    }
 }
